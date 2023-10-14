ed3a5a9eae5f5456b6a95f24319e526bda0e3af2
NiebexShasa <shasa@niebex.com>
2023-10-14 23:13:53 +0300

    pro apis

--- src/app.ts
+++ src/app.ts
@@ -2,11 +2,12 @@ import express from 'express'
 import config from 'config'
 import connect from './utils/connect'
 import routes from './routes'
-
+import morgan from 'morgan'
 
 const port = config.get<number>('port')
 
 const app = express()
+app.use(morgan("dev"))
 app.use(express.json())
 app.listen(port, async()=>{
     console.log(`📚 The app is running on http://localhost:${port}`)
